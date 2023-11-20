4ecb6588ebee38116d03daf270f2ffa450162b89
Irvan Malik <irvanmalik48@gmail.com>
2023-11-20 13:47:34 +0700

    feat(config): adjusted site title
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/site.config.ts
+++ src/site.config.ts
@@ -4,7 +4,7 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Fernanda Ma'rouf",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "realm.",
+	title: "akiekano.",
 	// Meta property used as the default description meta property
 	description: "Fernanda's personal website",
 	// HTML lang property, found in src/layouts/Base.astro L:18
